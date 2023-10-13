7a7238fd8e22e4ea5f50086a15ce95b915614186
DESKTOP-QKMASEC\Pegiadis <ioannispegiadis97@gmail.com>
2023-10-13 17:47:58 +0300

    Add more CRUD operations to the ActorOperations

--- DvdApi/DatabaseOperations/ActorOperations.cs
+++ DvdApi/DatabaseOperations/ActorOperations.cs
@@ -45,27 +45,94 @@ namespace DvdApi.DatabaseOperations
             return actors;
         }
 
-        internal Task<bool> AddActorAsync(Actor actor)
+        public async Task<Actor> GetActorByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "SELECT * FROM public.actor WHERE actor_id = @id";
+                var command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        return new Actor
+                        {
+                            ActorId = reader.GetInt32(reader.GetOrdinal("actor_id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("first_name")),
+                            LastName = reader.GetString(reader.GetOrdinal("last_name")),
+                            LastUpdate = reader.GetDateTime(reader.GetOrdinal("last_update"))
+                        };
+                    }
+                }
+            }
+
+            return null; // Return null if no actor was found with the provided ID
         }
 
-        internal Task<bool> DeleteActorAsync(int id)
+        public async Task<Actor> AddActorAsync(Actor actor)
         {
-            throw new NotImplementedException();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                // Include the "RETURNING *" clause to retrieve the inserted record
+                string query = "INSERT INTO public.actor (first_name, last_name, last_update) VALUES (@first_name, @last_name, @last_update) RETURNING *";
+                var command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@first_name", actor.FirstName);
+                command.Parameters.AddWithValue("@last_name", actor.LastName);
+                command.Parameters.AddWithValue("@last_update", actor.LastUpdate);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        // Assuming actor_id is auto-incremented by the database and is fetched here
+                        return new Actor
+                        {
+                            ActorId = reader.GetInt32(reader.GetOrdinal("actor_id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("first_name")),
+                            LastName = reader.GetString(reader.GetOrdinal("last_name")),
+                            LastUpdate = reader.GetDateTime(reader.GetOrdinal("last_update"))
+                        };
+                    }
+                }
+            }
+            return null; // if no record was inserted, return null
         }
 
-        internal Task<Actor> GetActorByIdAsync(int id)
+
+        public async Task<bool> UpdateActorAsync(Actor actor)
         {
-            throw new NotImplementedException();
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "UPDATE public.actor SET first_name = @first_name, last_name = @last_name, last_update = @last_update WHERE actor_id = @id";
+                var command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", actor.ActorId);
+                command.Parameters.AddWithValue("@first_name", actor.FirstName);
+                command.Parameters.AddWithValue("@last_name", actor.LastName);
+                command.Parameters.AddWithValue("@last_update", actor.LastUpdate);
+
+                var affectedRows = await command.ExecuteNonQueryAsync();
+                return affectedRows > 0;
+            }
         }
 
-        internal Task<bool> UpdateActorAsync(Actor actor)
+        public async Task<bool> DeleteActorAsync(int id)
         {
-            throw new NotImplementedException();
-        }
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "DELETE FROM public.actor WHERE actor_id = @id";
+                var command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", id);
 
-        // Similar methods for other operations (Create, Update, Delete, etc.) and for the 'Film' entity
+                var affectedRows = await command.ExecuteNonQueryAsync();
+                return affectedRows > 0;
+            }
+        }
     }
 
 }
