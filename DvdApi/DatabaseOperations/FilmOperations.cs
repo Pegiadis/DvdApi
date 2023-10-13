20fedfefa03d8c85dbdee1b84fb570bfe275bfe8
DESKTOP-QKMASEC\Pegiadis <ioannispegiadis97@gmail.com>
2023-10-13 17:50:36 +0300

    Add more CRUD operations for the FIlm sql table

--- DvdApi/DatabaseOperations/FilmOperations.cs
+++ DvdApi/DatabaseOperations/FilmOperations.cs
@@ -11,6 +11,8 @@ namespace DvdApi.DatabaseOperations
         {
             connectionString = DatabaseConnection.GetConnectionString();
         }
+
+        // Example method to get all films
         public async Task<List<Film>> GetAllFilmsAsync()
         {
             var films = new List<Film>();
@@ -37,6 +39,87 @@ namespace DvdApi.DatabaseOperations
             }
              
             return films;
+        }
+
+        public async Task<Film> GetFilmAsync(int id)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "SELECT * FROM public.film WHERE film_id = @id";
+                var command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        return new Film
+                        {
+                            FilmId = reader.GetInt32(reader.GetOrdinal("film_id")),
+                            // ... other fields
+                            LastUpdate = reader.GetDateTime(reader.GetOrdinal("last_update"))
+                        };
+                    }
+                }
+            }
+            return null; // Return null if no film was found with the provided ID
+        }
+
+        public async Task<Film> AddFilmAsync(Film film)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                //string query = "INSERT INTO public.film (/* fields except film_id */) VALUES (/* @parameters */) RETURNING film_id";
+                //var command = new NpgsqlCommand(query, connection);
+
+                string query = "INSERT INTO public.film (title, description) VALUES (@title, @description)";
+                using (var command = new NpgsqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("title", film.Title);
+                    command.Parameters.AddWithValue("description", film.Description);
+
+                    // Execute the command
+                    await command.ExecuteNonQueryAsync();
+                }
+
+
+                // Execute the query and get the ID of the newly inserted row
+                //film.FilmId = (int)await command.ExecuteScalarAsync();
+            }
+            return film; // Return the film with the newly assigned ID
+        }
+
+        public async Task<bool> UpdateFilmAsync(Film film)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "UPDATE public.film SET /* fields = @parameters, except film_id */ WHERE film_id = @id";
+                var command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", film.FilmId);
+
+                // Add command.Parameters.AddWithValue() for each field to be updated
+                // ...
+
+                var affectedRows = await command.ExecuteNonQueryAsync();
+                return affectedRows > 0; // Return true if the update was successful
+            }
+        }
+
+        public async Task<bool> DeleteFilmAsync(int id)
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "DELETE FROM public.film WHERE film_id = @id";
+                var command = new NpgsqlCommand(query, connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                var affectedRows = await command.ExecuteNonQueryAsync();
+                return affectedRows > 0; // Return true if the delete was successful
+            }
         }
     }
 }
--- DvdApi/Services/FilmService.cs
+++ DvdApi/Services/FilmService.cs
@@ -1,5 +1,6 @@
-﻿using DvdApi.Models;
-using DvdApi.DatabaseOperations;
+﻿using DvdApi.DatabaseOperations;
+using DvdApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,18 +10,101 @@ namespace DvdApi.Services
     {
         private readonly FilmOperations _filmOperations;
 
-        public FilmService(FilmOperations filmOperations)
+        public FilmService()
         {
-            _filmOperations = filmOperations;
+            _filmOperations = new FilmOperations(); // Consider dependency injection for this dependency.
         }
 
-        public Task<List<Film>> GetAllFilmsAsync()
+        public async Task<List<Film>> GetAllFilmsAsync()
         {
-            // You can add business logic here if needed
-            return _filmOperations.GetAllFilmsAsync();
+            try
+            {
+                return await _filmOperations.GetAllFilmsAsync();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                // Consider how you want to handle exceptions - rethrow, return null, return a default value, etc.
+                throw; // or handle differently according to your requirements
+            }
         }
 
-        // Add other methods corresponding to CRUD operations here
-        // ...
+        public async Task<Film> GetFilmAsync(int id)
+        {
+            if (id <= 0)
+            {
+                // Log and handle the error appropriately
+                // You might want to throw an exception or return null
+                throw new ArgumentException("Invalid ID", nameof(id));
+            }
+
+            try
+            {
+                return await _filmOperations.GetFilmAsync(id);
+            }
+            catch (Exception ex)
+            {
+                // Log and handle the exception
+                throw; // or handle differently
+            }
+        }
+
+        public async Task<Film> CreateFilmAsync(Film film)
+        {
+            if (film == null)
+            {
+                throw new ArgumentNullException(nameof(film));
+            }
+
+            // You might want to add further validation here
+
+            try
+            {
+                return await _filmOperations.AddFilmAsync(film);
+            }
+            catch (Exception ex)
+            {
+                // Log and handle the exception
+                throw; // or handle differently
+            }
+        }
+
+        public async Task<bool> UpdateFilmAsync(Film film)
+        {
+            if (film == null)
+            {
+                throw new ArgumentNullException(nameof(film));
+            }
+
+            // Further validation
+
+            try
+            {
+                return await _filmOperations.UpdateFilmAsync(film);
+            }
+            catch (Exception ex)
+            {
+                // Log and handle the exception
+                throw; // or handle differently
+            }
+        }
+
+        public async Task<bool> DeleteFilmAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Invalid ID", nameof(id));
+            }
+
+            try
+            {
+                return await _filmOperations.DeleteFilmAsync(id);
+            }
+            catch (Exception ex)
+            {
+                // Log and handle the exception
+                throw; // or handle differently
+            }
+        }
     }
 }
