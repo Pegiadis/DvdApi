81cb4aa7a402344af6968fe993e04d1ae44601db
DESKTOP-QKMASEC\Pegiadis <username@users.noreply.github.com>
2023-10-31 11:47:01 +0200

    final changes to release

--- DvdApi/DatabaseConnection.cs
+++ DvdApi/DatabaseConnection.cs
@@ -1,17 +1,40 @@
-﻿namespace DvdApi;
+﻿
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
-public static class DatabaseConnection
+namespace DvdApi
 {
-    public static string? GetConnectionString()
+    public static class DatabaseConnection
     {
-        // Build configuration
-        var configurationBuilder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory()) // Directory where the json files are located
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        public static string? GetConnectionString()
+        {
+            // Build configuration
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Directory where the json files are located
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-        IConfigurationRoot configurationRoot = configurationBuilder.Build();
+            IConfigurationRoot configurationRoot = configurationBuilder.Build();
 
-        // Retrieve connection string from appsettings.json
-        return configurationRoot.GetConnectionString("PostgreSqlConnection");
+            // Retrieve connection string from appsettings.json
+            string? connectionStringTemplate = configurationRoot.GetConnectionString("PostgreSqlConnection");
+
+            // Get password from environment variable
+            string? password = Environment.GetEnvironmentVariable("DB_DVD");
+
+            if (connectionStringTemplate != null && password != null)
+            {
+                // Replace the placeholder with the actual password
+                return connectionStringTemplate.Replace("{password}", password);
+            }
+            else
+            {
+                // Handle the case where either the connection string or password is missing.
+                // You can throw an exception, return null, or handle it in another way depending on your application needs.
+                throw new InvalidOperationException("Connection string or password is missing.");
+            }
+        }
     }
 }
+
+
--- DvdApi/Services/ActorService.cs
+++ DvdApi/Services/ActorService.cs
@@ -5,6 +5,14 @@ using System.Threading.Tasks;
 
 namespace DvdApi.Services
 {
+    public interface IActorService
+    {
+        Task<List<Actor>> GetAllActorsAsync();
+        Task<Actor> GetActorByIdAsync(int id);
+        Task<Actor> AddActorAsync(Actor actor);
+        Task<bool> UpdateActorAsync(Actor actor);
+        Task<bool> DeleteActorAsync(int id);
+    }
     public class ActorService : IActorService
     {
         private readonly ActorOperations _dbAccess;
