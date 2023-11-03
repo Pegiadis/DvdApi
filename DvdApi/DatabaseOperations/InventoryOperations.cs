57b05e68721ff02846627efc1796cc9d62fee5c6
pegiadis <97729400+Pegiadis@users.noreply.github.com>
2023-11-03 17:17:02 +0200

    Feature/add more controllers (#18)
    Add more controllers for the rest of the tables in the db

--- DvdApi/DatabaseOperations/InventoryOperations.cs
+++ DvdApi/DatabaseOperations/InventoryOperations.cs
@@ -41,5 +41,99 @@ namespace DvdApi.DatabaseOperations
             
         }
 
+        public async Task<Inventory> GetInventoryByIdAsync(int id)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = "SELECT * FROM public.inventory WHERE inventory_id = @id";
+
+                var command = new NpgsqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@id", id);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        return new Inventory
+                        {
+                            InventoryId = reader.GetInt32(reader.GetOrdinal("inventory_id")),
+                            FilmId = reader.GetInt32(reader.GetOrdinal("film_id")),
+                            StoreId = reader.GetInt32(reader.GetOrdinal("store_id")),
+                            LastUpdate = reader.GetDateTime(reader.GetOrdinal("last_update"))
+                        };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+
+        public async Task<Inventory> AddInventoryAsync(Inventory inventory)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = "INSERT INTO public.inventory (film_id, store_id, last_update) VALUES (@film_id, @store_id, @last_update) RETURNING inventory_id";
+
+                var command = new NpgsqlCommand(query, connection);
+                
+                command.Parameters.AddWithValue("@id", inventory.InventoryId);
+                command.Parameters.AddWithValue("@film_id", inventory.FilmId);
+                command.Parameters.AddWithValue("@store_id", inventory.StoreId);
+                command.Parameters.AddWithValue("@last_update", inventory.LastUpdate);
+
+                var id = (int)await command.ExecuteScalarAsync();
+
+                inventory.InventoryId = id;
+
+                return inventory;
+            }
+        }
+
+
+        public async Task<Inventory> UpdateInventoryAsync(Inventory inventory)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = "UPDATE public.inventory SET film_id = @film_id, store_id = @store_id, last_update = @last_update WHERE inventory_id = @id";
+
+                var command = new NpgsqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@id", inventory.InventoryId);
+                command.Parameters.AddWithValue("@film_id", inventory.FilmId);
+                command.Parameters.AddWithValue("@store_id", inventory.StoreId);
+                command.Parameters.AddWithValue("@last_update", inventory.LastUpdate);
+
+                await command.ExecuteNonQueryAsync();
+
+                return inventory;
+            }
+        }
+
+
+        public async Task<bool> DeleteInventoryAsync(int id)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+
+                string query = "DELETE FROM public.inventory WHERE inventory_id = @id";
+
+                var command = new NpgsqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@id", id);
+
+                var affectedRows = await command.ExecuteNonQueryAsync();
+                return affectedRows > 0;
+            }
+        }
+
     }
 }
